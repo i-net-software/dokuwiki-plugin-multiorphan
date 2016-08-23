9083f0d9f8874b0af2fbf0ed41b544cb049cdacd
rnck <github@rnck.de>
2016-08-23 12:44:52 +0200

    correct links to plugin repo, updated translation

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -5,35 +5,36 @@
  * 
  * @author Padhie <develop@padhie.de>
  * @author liz <marliza@web.de>
+ * @author rnck <dokuwiki@rnck.de>
  */
-$lang['encoding']              = 'UTF-8';
-$lang['direction']             = 'ltr';
-$lang['startingNamespace']     = 'Wähle dein Anfangsnamespace';
-$lang['startProcess']          = 'Prozess anfangen';
-$lang['start']                 = 'Anfang';
-$lang['status']                = 'Status';
-$lang['ns']                    = 'Setze Namespace';
-$lang['idFilter']              = 'RegEx um vollständige ID zu filtern';
-$lang['purge']                 = 'Cache von geprüfter ID bereinigen';
-$lang['depth']                 = 'Tiefe';
-$lang['depthType']             = 'Schrift exportieren';
-$lang['depth.pageOnly']        = 'Nur diese Seite';
-$lang['depth.allSubNameSpaces'] = 'Alle Unternamespaces';
-$lang['depth.specifiedDepth']  = 'angegebene Tiefe';
-$lang['type']                  = 'Was soll geprüft werden';
-$lang['type.pages']            = 'Nur Seiten';
-$lang['type.media']            = 'Nur Mediadateien';
-$lang['type.both']             = 'Seiten und Mediadateien';
-$lang['pages-result']          = 'Ergebnis PAGES Check';
-$lang['media-result']          = 'Ergebnis MEDIA Check';
-$lang['wanted']                = 'Benötigt';
-$lang['orphan']                = 'Verwaist';
-$lang['linked']                = 'Verlinkt';
-$lang['js']['request-aborted'] = 'Anfrage abgebrochen';
-$lang['js']['start']           = 'Start';
-$lang['js']['stop']            = 'Stop';
-$lang['js']['checking-done']   = 'Überprüfen der Seiten erledigt';
-$lang['js']['checking-page']   = 'Überprüfe Seite';
-$lang['js']['checking-orphans'] = 'Suche nach verwaisten Dateien.';
-$lang['js']['error-parsing']   = 'Fehler Antwort Syntaxanalyse:';
+$lang['encoding']                 = 'UTF-8';
+$lang['direction']                = 'ltr';
+$lang['startingNamespace']        = 'Wähle dein Anfangsnamensraum';
+$lang['startProcess']             = 'Prozess starten';
+$lang['start']                    = 'Start';
+$lang['status']                   = 'Status';
+$lang['ns']                       = 'Setze Namensraum';
+$lang['idFilter']                 = 'RegEx um vollständige ID zu filtern';
+$lang['purge']                    = 'Cache von geprüfter ID bereinigen';
+$lang['depth']                    = 'Tiefe';
+$lang['depthType']                = 'Schrift exportieren';
+$lang['depth.pageOnly']           = 'Nur diese Seite';
+$lang['depth.allSubNameSpaces']   = 'Alle Unternamespaces';
+$lang['depth.specifiedDepth']     = 'angegebene Tiefe';
+$lang['type']                     = 'Was soll geprüft werden';
+$lang['type.pages']               = 'Nur Seiten';
+$lang['type.media']               = 'Nur Mediadateien';
+$lang['type.both']                = 'Seiten und Mediadateien';
+$lang['pages-result']             = 'Ergebnis der Seitenüberprüfung';
+$lang['media-result']             = 'Ergebnis der Medienüberprüfung';
+$lang['wanted']                   = 'Benötigt';
+$lang['orphan']                   = 'Verwaist';
+$lang['linked']                   = 'Verlinkt';
+$lang['js']['request-aborted']    = 'Anfrage abgebrochen';
+$lang['js']['start']              = 'Start';
+$lang['js']['stop']               = 'Stop';
+$lang['js']['checking-done']      = 'Überprüfen der Seiten erledigt';
+$lang['js']['checking-page']      = 'Überprüfe Seite';
+$lang['js']['checking-orphans']   = 'Suche nach verwaisten Dateien.';
+$lang['js']['error-parsing']      = 'Fehler Antwort Syntaxanalyse:';
 $lang['js']['please-wait-orphan'] = 'Bitte warte bis der Prozess beendet wurde.';
