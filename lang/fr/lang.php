6c92d9cd40a0389221ee24e5d2db8402e895fe0d
ubibene <services.m@benard.info>
2017-05-25 08:35:13 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -2,33 +2,36 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Nicolas Friedli <nicolas@theologique.ch>
+ * @author ubibene <services.m@benard.info>
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr (gauche à droite)';
 $lang['menu']                  = 'multiORPHANS';
 $lang['startingNamespace']     = 'Entrez l\'espace de nom de départ';
-$lang['startProcess']          = 'Démarrage du processus';
+$lang['startProcess']          = 'Démarrer le processus';
 $lang['start']                 = 'Démarrer';
 $lang['status']                = 'Statut';
 $lang['ns']                    = 'Attribuer l\'espace de nom';
-$lang['idFilter']              = 'Expression régulière pour filter l\'ID';
-$lang['purge']                 = 'Effacer le cache pour l\'ID donnée';
+$lang['idFilter']              = 'Expression régulière pour filtrer l\'ID';
+$lang['purge']                 = 'Effacer le cache pour l\'ID sélectionné';
 $lang['depth']                 = 'Profondeur';
 $lang['depthType']             = 'Type d\'exportation';
 $lang['depth.pageOnly']        = 'cette page seulement';
-$lang['depth.allSubNameSpaces'] = 'tous les sous espaces de nom';
+$lang['depth.allSubNameSpaces'] = 'tous les sous-espaces de nom';
 $lang['depth.specifiedDepth']  = 'profondeur précisée';
 $lang['type']                  = 'Que vérifier';
 $lang['type.pages']            = 'Pages seulement';
 $lang['type.media']            = 'Médias seulement';
 $lang['type.both']             = 'Pages et médias';
-$lang['pages-result']          = 'Résultat de la vérification des pages';
-$lang['media-result']          = 'Résultat de la vérification des médias';
+$lang['pages-result']          = 'Résultat de la vérification des PAGES';
+$lang['media-result']          = 'Résultat de la vérification des MEDIAS';
 $lang['wanted']                = 'Demandé';
 $lang['orphan']                = 'Orphelin';
 $lang['linked']                = 'Lié';
+$lang['checkExternal']         = 'Vérifie les URLs externes.';
+$lang['checkExternalHint']     = 'Dégrade les performances à cause de la vérification du statut HTTP "200 OK" de pages externes.';
 $lang['js']['request-aborted'] = 'Requête abandonnée';
 $lang['js']['start']           = 'Démarrer';
 $lang['js']['stop']            = 'Arrêter';
