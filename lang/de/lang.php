d157a80edb284de25910981f4d031aea3a080354
F. Mueller-Donath <j.felix@mueller-donath.de>
2017-10-12 10:15:35 +0200

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -2,41 +2,43 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author F. Mueller-Donath <j.felix@mueller-donath.de>
  * @author Padhie <develop@padhie.de>
  * @author liz <marliza@web.de>
  * @author rnck <dokuwiki@rnck.de>
  */
-$lang['encoding']                 = 'UTF-8';
-$lang['direction']                = 'ltr';
-$lang['startingNamespace']        = 'Wähle dein Anfangsnamensraum';
-$lang['startProcess']             = 'Prozess starten';
-$lang['start']                    = 'Start';
-$lang['status']                   = 'Status';
-$lang['ns']                       = 'Setze Namensraum';
-$lang['idFilter']                 = 'RegEx um vollständige ID zu filtern';
-$lang['purge']                    = 'Cache von geprüfter ID bereinigen';
-$lang['depth']                    = 'Tiefe';
-$lang['depthType']                = 'Schrift exportieren';
-$lang['depth.pageOnly']           = 'Nur diese Seite';
-$lang['depth.allSubNameSpaces']   = 'Alle Unternamespaces';
-$lang['depth.specifiedDepth']     = 'angegebene Tiefe';
-$lang['type']                     = 'Was soll geprüft werden';
-$lang['type.pages']               = 'Nur Seiten';
-$lang['type.media']               = 'Nur Mediadateien';
-$lang['type.both']                = 'Seiten und Mediadateien';
-$lang['pages-result']             = 'Ergebnis der Seitenüberprüfung';
-$lang['media-result']             = 'Ergebnis der Medienüberprüfung';
-$lang['wanted']                   = 'Benötigt';
-$lang['orphan']                   = 'Verwaist';
-$lang['linked']                   = 'Verlinkt';
-$lang['checkExternal']            = 'Externe URLs prüfen';
-$lang['includeHidden']            = 'Versteckte Seiten prüfen';
-$lang['js']['request-aborted']    = 'Anfrage abgebrochen';
-$lang['js']['start']              = 'Start';
-$lang['js']['stop']               = 'Stop';
-$lang['js']['checking-done']      = 'Überprüfen der Seiten erledigt';
-$lang['js']['checking-page']      = 'Überprüfe Seite';
-$lang['js']['checking-orphans']   = 'Suche nach verwaisten Dateien.';
-$lang['js']['error-parsing']      = 'Fehler Antwort Syntaxanalyse:';
-$lang['js']['please-wait-orphan'] = 'Bitte warte bis der Prozess beendet wurde.';
+$lang['encoding']              = 'UTF-8';
+$lang['direction']             = 'ltr';
+$lang['startingNamespace']     = 'Gib einen Namensraum, der durchsucht wird, an';
+$lang['startProcess']          = 'Prozess starten';
+$lang['start']                 = 'Start';
+$lang['status']                = 'Status';
+$lang['ns']                    = 'Setze Namensraum';
+$lang['idFilter']              = 'RegEx um vollständige ID zu filtern';
+$lang['purge']                 = 'Cache von geprüfter ID bereinigen';
+$lang['depth']                 = 'Tiefe';
+$lang['depthType']             = 'Schrift exportieren';
+$lang['depth.pageOnly']        = 'Nur diese Seite';
+$lang['depth.allSubNameSpaces'] = 'Alle Unternamensräume';
+$lang['depth.specifiedDepth']  = 'angegebene Tiefe';
+$lang['type']                  = 'Was soll geprüft werden';
+$lang['type.pages']            = 'Nur Seiten';
+$lang['type.media']            = 'Nur Mediadateien';
+$lang['type.both']             = 'Seiten und Mediadateien';
+$lang['pages-result']          = 'Ergebnis der Seitenüberprüfung';
+$lang['media-result']          = 'Ergebnis der Medienüberprüfung';
+$lang['wanted']                = 'Benötigt';
+$lang['orphan']                = 'Verwaist';
+$lang['linked']                = 'Verlinkt';
+$lang['checkExternal']         = 'Externe URLs prüfen';
+$lang['includeHidden']         = 'Versteckte Seiten prüfen';
+$lang['checkExternalHint']     = 'Beansprucht etwas Performance, da externe Websites auf HTTP-Status "200 OK" überprüft werden.';
+$lang['js']['request-aborted'] = 'Anfrage abgebrochen';
+$lang['js']['start']           = 'Start';
+$lang['js']['stop']            = 'Stop';
+$lang['js']['checking-done']   = 'Überprüfen der Seiten erledigt';
+$lang['js']['checking-page']   = 'Überprüfe Seite';
+$lang['js']['checking-orphans'] = 'Suche nach verwaisten Dateien.';
+$lang['js']['error-parsing']   = 'Fehler Antwort Syntaxanalyse:';
+$lang['js']['please-wait-orphan'] = 'Bitte warten Sie bis der Prozess beendet wurde.';
