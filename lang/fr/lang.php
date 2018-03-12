05db6a9179b0d04fd2752f8764b12a2d1ce3f809
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2018-03-12 07:45:18 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -3,23 +3,24 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
  * @author Nicolas Friedli <nicolas@theologique.ch>
  * @author ubibene <services.m@benard.info>
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr (gauche à droite)';
 $lang['menu']                  = 'multiORPHANS';
-$lang['startingNamespace']     = 'Entrez l\'espace de nom de départ';
+$lang['startingNamespace']     = 'Entrez la catégorie de départ';
 $lang['startProcess']          = 'Démarrer le processus';
 $lang['start']                 = 'Démarrer';
 $lang['status']                = 'Statut';
-$lang['ns']                    = 'Attribuer l\'espace de nom';
+$lang['ns']                    = 'Catégorie';
 $lang['idFilter']              = 'Expression régulière pour filtrer l\'ID';
 $lang['purge']                 = 'Effacer le cache pour l\'ID sélectionné';
 $lang['depth']                 = 'Profondeur';
 $lang['depthType']             = 'Type d\'exportation';
 $lang['depth.pageOnly']        = 'cette page seulement';
-$lang['depth.allSubNameSpaces'] = 'tous les sous-espaces de nom';
+$lang['depth.allSubNameSpaces'] = 'toutes les sous-catégories';
 $lang['depth.specifiedDepth']  = 'profondeur précisée';
 $lang['type']                  = 'Que vérifier';
 $lang['type.pages']            = 'Pages seulement';
@@ -27,16 +28,17 @@ $lang['type.media']            = 'Médias seulement';
 $lang['type.both']             = 'Pages et médias';
 $lang['pages-result']          = 'Résultat de la vérification des PAGES';
 $lang['media-result']          = 'Résultat de la vérification des MEDIAS';
-$lang['wanted']                = 'Demandé';
-$lang['orphan']                = 'Orphelin';
-$lang['linked']                = 'Lié';
+$lang['wanted']                = 'Souhaités';
+$lang['orphan']                = 'Orphelins';
+$lang['linked']                = 'Liés';
 $lang['checkExternal']         = 'Vérifie les URLs externes.';
+$lang['includeHidden']         = 'Inclure les pages cachées';
 $lang['checkExternalHint']     = 'Dégrade les performances à cause de la vérification du statut HTTP "200 OK" de pages externes.';
 $lang['js']['request-aborted'] = 'Requête abandonnée';
 $lang['js']['start']           = 'Démarrer';
 $lang['js']['stop']            = 'Arrêter';
 $lang['js']['checking-done']   = 'Inspection des pages terminée';
 $lang['js']['checking-page']   = 'Inspection de la page';
-$lang['js']['checking-orphans'] = 'Vérifie les orphelins.';
-$lang['js']['error-parsing']   = 'Réponse d\'erreur de l\'analyse.';
+$lang['js']['checking-orphans'] = 'Recherche des orphelins.';
+$lang['js']['error-parsing']   = 'Erreur de l\'analyse de la réponse.';
 $lang['js']['please-wait-orphan'] = 'Merci d\'attendre la fin du processus';
