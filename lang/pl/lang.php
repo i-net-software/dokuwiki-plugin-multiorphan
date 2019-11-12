0ba68b826c9baf0b54182e463c089893146dbf08
Bartek S <sadupl@gmail.com>
2019-11-12 10:35:35 +0100

    translation update

---
+++ lang/pl/lang.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['encoding']              = 'utf-8	';
+$lang['direction']             = 'ltr';
+$lang['menu']                  = 'multiSieroty';
+$lang['start']                 = 'Start';
+$lang['status']                = 'Status';
+$lang['depth']                 = 'Głębokość';
+$lang['depthType']             = 'Typ eksportu';
+$lang['depth.pageOnly']        = 'tylko ta strona';
+$lang['depth.specifiedDepth']  = 'określona głębokość';
+$lang['type']                  = 'Co sprawdzić';
+$lang['type.pages']            = 'Tylko strony';
+$lang['type.media']            = 'Tylko media';
+$lang['type.both']             = 'Strony i media';
+$lang['pages-result']          = 'Wynik sprawdzania stron';
+$lang['media-result']          = 'Wynik sprawdzania mediów';
+$lang['includeHidden']         = 'Uwzględnij ukryte strony';
+$lang['js']['start']           = 'Start';
+$lang['js']['stop']            = 'Stop';
+$lang['js']['checking-done']   = 'Zakończono sprawdzanie stron';
+$lang['js']['checking-page']   = 'Sprawdzam stronę';
+$lang['js']['checking-orphans'] = 'Sprawdzanie sierot.';
